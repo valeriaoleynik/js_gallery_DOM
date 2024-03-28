d566fbd4c7e783b0d8d78658285e7d4cf016af05
Valeria Oleynik <oleynik.valeria27@gmail.com>
2024-03-28 15:25:19 +0200

    make build

---
+++ index.f95e6047.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg"),t=document.querySelector(".gallery__list");t.addEventListener("click",function(r){r.preventDefault();var l=r.target.closest(".list-item__link");l&&t.contains(l)&&(e.src=l.href)});
+//# sourceMappingURL=index.f95e6047.js.map
